95dc10839d6a6fcd211d0438d060fdcf821f9641
Alfian Pratama <alfianpratama@hotmail.com>
2023-01-05 11:04:48 +0700

    improve

--- tiktokshop.py
+++ tiktokshop.py
@@ -4,6 +4,7 @@ import pandas as pd
 import time
 from selenium.webdriver.support import expected_conditions as EC
 from appium.webdriver.common.touch_action import TouchAction
+import requests
 
 LOOP = 2
 
@@ -46,57 +47,56 @@ screenHeight = deviceSize['height']
 startx = screenWidth/2
 endx = screenWidth/2
 starty = screenHeight*8/9
-endy = screenHeight/10
+endy = screenHeight/8
 
 actions = TouchAction(driver)
 
 def get_link():
-    time.sleep(4)
+    time.sleep(2)
     el11 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ImageView")
     el11.click()
-    time.sleep(3)
+    time.sleep(1)
     el21 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]")
     el21.click()
     text = driver.get_clipboard_text()
-    print (text)
     return text
 
 df = []
-def first_click():
-    first = {
+def open_product():
+    xy = {
         279 : 600,
         864 : 600,
-    }
-    for i, j in first:
-        actions.tap(None,i,j,1)
-        actions.perform()
-        get_link()
-        df.append(get_link())
-        driver.back()
-
-def second_click():
-    second = {
         268 : 1362,
         786 : 1362,
         264 : 2201,
         823 : 2201
     }
-    for i, j in second:
+    for i, j in xy.items():
         actions.tap(None,i,j,1)
         actions.perform()
-        get_link()
-        df.append(get_link())
+        link = get_link()
+        print("found link : ", link)
+        df.append(link)
         driver.back()
 
-
-i = 1
-while i <= LOOP:
-    if i < 1:
+try:
+    i = 1
+    while i <= LOOP:
         actions.long_press(None,startx,starty).move_to(None,endx,endy).release().perform()
-        first_click()
-        second_click()
-    else:
-        actions.long_press(None,startx,starty).move_to(None,endx,endy).release().perform()
-        second_click()
-    i = i+1
+        open_product()
+        i = i+1
+except:
+    pass
 
+result = []
+session = requests.Session()  # so connections are recycled
+
+url = []
+for i in df:
+    resp = session.head(i, allow_redirects=True)
+    url.append(resp.url)
+
+[result.append(x) for x in url if x not in result]
+df_result = pd.DataFrame(result)
+df_result.to_csv("test.csv")
+print(df_result)
