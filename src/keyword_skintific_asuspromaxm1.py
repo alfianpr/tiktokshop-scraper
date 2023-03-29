595ac1bef67a6284d4584eba479cba89b054f384
Alfian Pratama <alfianpratama@hotmail.com>
2023-03-29 17:17:30 +0700

    improve code

--- src/keyword_skintific_asuspromaxm1.py
+++ src/keyword_skintific_asuspromaxm1.py
@@ -9,6 +9,7 @@ CONNECTION = ASUSPROMAXM1
 SKIP = False  # First time set to False, set True if you want to continue the process
 SERVER_APPIUM_PORT = "4723"
 SERVER_APPIUM_IP = "127.0.0.1"
+SCRAPE_METHOD = "ELEMENT"
 
 DESIRED_CAPS = {
     "platformName": "Android",
@@ -23,7 +24,11 @@ driver = driver(SERVER_APPIUM_IP=SERVER_APPIUM_IP, SERVER_APPIUM_PORT=SERVER_APP
 k = 0
 while k <= SCROLL_LOOP:
     print (f"Scrape the loop at {k}")
-    time.sleep(2); open_product_v2_search_asuspromaxm1(CATEGORY=CATEGORY, SKIP=SKIP, k=k)
+    match SCRAPE_METHOD:
+        case "ELEMENT":
+            time.sleep(2); open_product_v2_search_asuspromaxm1(CATEGORY=CATEGORY, SKIP=SKIP, k=k)
+        case "COORDINAT":
+            time.sleep(2); open_product_v1_search_asuspromaxm1(CATEGORY=CATEGORY)
     driver.swipe(SC_1[0], SC_1[1], SC_1[2], SC_1[3], SC_1[4]) # Adjust with your device
     time.sleep(2)
     try: close_dialog()
