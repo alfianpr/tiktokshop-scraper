835ddfb946cb18f8bc6984b0be40a7e61198da7c
alfanpr <alfanpratama@hotmail.com>
2023-04-27 00:17:10 +0700

    init

---
+++ src/utils_top.py
@@ -0,0 +1,63 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+import pandas as pd
+import time
+from appium.webdriver.common.touch_action import TouchAction
+
+# Setup connection
+ASUSPROMAXM1 = "192.168.0.111:5555"
+
+# Setup Component for Asus Pro Max M1
+SHARE_BUTTON = "com.ss.android.ugc.trill:id/hog"
+COPY_BUTTON = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]"
+CLOSE_DIALOG = "com.ss.android.ugc.trill:id/f54"
+UP_BUTTON = "com.ss.android.ugc.trill:id/c4f"
+BACK_BUTTON = "com.ss.android.ugc.trill:id/a2f"
+
+# Setup coordinat
+"""
+Setup Coordinat for Asus Pro Max M1
+Structure (start_x, start_y, end_x, end_y, speed)
+"""
+SCROLL_REFRESH_CAT_PAGE = [500, 465, 500, 948, 400]
+SELECT_PRODUCT = {500 : 550, 500 : 875, 500 : 1222, 500 : 1560, 500 : 1900}
+SCROLL_DOWN = [500, 1900, 500, 400, 400]
+
+# Setup the Appium driver
+def driver(SERVER_APPIUM_IP, SERVER_APPIUM_PORT, desired_caps):
+        global driver
+        driver = webdriver.Remote(f"http://{SERVER_APPIUM_IP}:{SERVER_APPIUM_PORT}/wd/hub", desired_caps)
+        return driver
+
+# close unnecesary dialogbox
+def close_dialog(): 
+    return driver.find_element(by=AppiumBy.ID, value=f"{CLOSE_DIALOG}").click() 
+
+# Click share link and copy
+def get_link_cat_asuspromaxm1():
+    time.sleep(2)
+    driver.find_element(by=AppiumBy.ID, value=f"{SHARE_BUTTON}").click()
+    time.sleep(1)
+    driver.find_element(by=AppiumBy.XPATH, value=f"{COPY_BUTTON}").click()
+    return driver.get_clipboard_text()
+
+# Open Product by Coordinat
+def open_product_v1_cat_asuspromaxm1(CATEGORY):
+    actions = TouchAction(driver)
+    df = [] 
+    # Click coordinat
+    for i, j in SELECT_PRODUCT.items():
+        try: actions.tap(None,i,j).perform()
+        except: continue
+        try: close_dialog()
+        except: pass
+        try:
+            link = get_link_cat_asuspromaxm1()
+            print("found link : ", link)
+            df.append(link)
+            driver.back(); continue
+        except: pass
+        try: driver.find_element(by=AppiumBy.ID, value=f"{BACK_BUTTON}").click(); print("back, wrong click!")
+        except: pass
+    df = pd.DataFrame(df)
+    df.to_csv(f'./url/{CATEGORY}.csv', mode='a', index=False, header=False)
