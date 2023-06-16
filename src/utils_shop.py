606881c7fb5b822313f962b18bdd9ce03fea9545
alfianpr <alfianpratama@hotmail.com>
2023-06-17 03:29:59 +0700

    update

--- src/utils_shop.py
+++ src/utils_shop.py
@@ -6,6 +6,11 @@ from appium.webdriver.common.appiumby import AppiumBy
 from appium.webdriver.common.touch_action import TouchAction
 import time
 
+# Input
+START_SHOP = 0
+CATEGORY = "Formula Milk & Baby Food"
+CONNECTION = ""
+
 # Sony Xperia XZ2
 SHOP_BUTTON = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.ImageView"
 OPEN_SHOP = '//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Visit store"]'
@@ -13,14 +18,12 @@ SELECT_PRODUCT = {210 : 805, 788 : 810, 211 : 1969, 791 : 1974}
 SCROLL_DOWN = [500, 1900, 500, 800, 400]
 SWIPE_PRODUCT = [540, 590, 540, 1850, 400]
 CLOSE_TOP_PRODUCT = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/com.lynx.tasm.behavior.ui.LynxFlattenUI[3]"
-SHARE_BUTTON_1 = "com.ss.android.ugc.trill:id/i3v"
+SHARE_BUTTON_1 = "com.ss.android.ugc.trill:id/ie_"
 SHARE_BUTTON_2 = "com.ss.android.ugc.trill:id/i3u"
 COPY_BUTTON = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.ImageView"
 KEYWORD_BOX = "com.ss.android.ugc.trill:id/c4v"
 SC_1 = SWIPE_PRODUCT
 SC_2 = SCROLL_DOWN
-
-CATEGORY = "test"
 
 byte_output = proc.check_output(["adb", "shell", "ip", "addr", "show", "wlan0", "|", "grep", "inet"])
 str_output = str(byte_output, "utf-8")
@@ -29,7 +32,7 @@ try: print("connected to: " + regex[0])
 except: print("please connect to device")
 
 shop_link = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_FJdKV7PMEwjO-GgOiK1DJlo6HsShHAW7gYY9saZjYSs_7oiP4uN28MqxXh_peDFRFY1p1vSs3tz/pub?gid=0&single=true&output=csv")
-link_shop = shop_link["Tiktok URL"]
+link_shop = shop_link["Tiktok URL"].iloc[START_SHOP:]
 
 desired_caps = {
     "platformName": "Android",
@@ -39,8 +42,10 @@ desired_caps = {
 }
 
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+actions = TouchAction(driver)
 
-def close_dialog(): return driver.find_element(by=AppiumBy.ID, value=f"{CLOSE_DIALOG}").click() # close unnecesary dialogbox
+def close_dialog(): 
+    return driver.find_element(by=AppiumBy.ID, value=f"{CLOSE_DIALOG}").click() # close unnecesary dialogbox
 
 def get_link_search():
     #driver.implicitly_wait(4)
@@ -54,7 +59,7 @@ def get_link_search():
     driver.find_element(by=AppiumBy.XPATH, value=f"{COPY_BUTTON}").click()
     return driver.get_clipboard_text()
 
-actions = TouchAction(driver)
+# Open product function
 def open_prod():
     df = []
     for i, j in SELECT_PRODUCT.items():
@@ -84,9 +89,11 @@ def open_prod():
         except: pass
     df = pd.DataFrame(df)
     df.to_csv(f'./url/{CATEGORY}.csv', mode='a', index=False, header=False)
+
+# Scroll and select products function
 def swipe():
     k = 1
-    while k<=30:
+    while k<=50:
         print (f"Scrape the loop at {k}")
         open_prod()
         driver.swipe(SC_2[0], SC_2[1], SC_2[2], SC_2[3], SC_2[4]) # Adjust with your device
@@ -97,7 +104,10 @@ def swipe():
             break
         except: pass
 
+# Main flow of program
+b = 0
 for i in link_shop:
+    print(f"opening shop {b}")
     try:
         driver.execute_script("mobile: deepLink", {'url': i, 'package': 'com.ss.android.ugc.trill'})
     except: continue
@@ -109,10 +119,18 @@ for i in link_shop:
     try:
         driver.find_element(by=AppiumBy.XPATH, value=f"{OPEN_SHOP}").click()
     except: continue
-    time.sleep(4)
-    try:
-        driver.find_element(by=AppiumBy.XPATH, value='//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Products"]').click()
-    except: continue
     time.sleep(3)
+    try: 
+        driver.find_element(by=AppiumBy.XPATH, value='//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Categories"]').click()
+        time.sleep(2)
+        driver.find_element(by=AppiumBy.XPATH, value=f'//*[contains(@text,"{CATEGORY}")]').click()
+    except: 
+        driver.find_element(by=AppiumBy.XPATH, value='//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Products"]').click()
+    time.sleep(3)
+    try:
+        driver.find_element(by=AppiumBy.XPATH, value='//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Bestsellers"]').click()
+    except: continue
+    time.sleep(2)
     swipe()
+    b = b+1
     driver.close_app()
