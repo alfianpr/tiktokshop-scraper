2638be4ec08161a15d9fe6a6ad28153c8554ee1c
alfanpr <alfanpratama@hotmail.com>
2023-07-04 01:48:38 +0700

    update

--- src/utils_shop.py
+++ src/utils_shop.py
@@ -7,20 +7,20 @@ from appium.webdriver.common.touch_action import TouchAction
 import time
 
 # Input
-START_SHOP = 0
-CATEGORY = "Formula Milk & Baby Food"
+START_SHOP = 2
+CATEGORY = "Prayer Attire & Equipment"
 CONNECTION = ""
 
 # Sony Xperia XZ2
 SHOP_BUTTON = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.ImageView"
 OPEN_SHOP = '//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Visit store"]'
 SELECT_PRODUCT = {210 : 805, 788 : 810, 211 : 1969, 791 : 1974}
-SCROLL_DOWN = [500, 1900, 500, 800, 400]
+SCROLL_DOWN = [500, 1900, 500, 800, 500]
 SWIPE_PRODUCT = [540, 590, 540, 1850, 400]
 CLOSE_TOP_PRODUCT = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/com.lynx.tasm.behavior.ui.LynxFlattenUI[3]"
-SHARE_BUTTON_1 = "com.ss.android.ugc.trill:id/ie_"
+SHARE_BUTTON_1 = "com.ss.android.ugc.trill:id/iko"
 SHARE_BUTTON_2 = "com.ss.android.ugc.trill:id/i3u"
-COPY_BUTTON = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.ImageView"
+COPY_BUTTON = "//android.widget.Button[@content-desc='Copy link']/android.widget.TextView"
 KEYWORD_BOX = "com.ss.android.ugc.trill:id/c4v"
 SC_1 = SWIPE_PRODUCT
 SC_2 = SCROLL_DOWN
@@ -31,8 +31,8 @@ regex = re.findall(r'(?<=inet ).*?(?=/)', str_output)
 try: print("connected to: " + regex[0])
 except: print("please connect to device")
 
-shop_link = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_FJdKV7PMEwjO-GgOiK1DJlo6HsShHAW7gYY9saZjYSs_7oiP4uN28MqxXh_peDFRFY1p1vSs3tz/pub?gid=0&single=true&output=csv")
-link_shop = shop_link["Tiktok URL"].iloc[START_SHOP:]
+shop_link = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT7tvXpj_f76tQ253mumt-PaGOH_Xn7qo1k-BFRNm4VppAaOgBipS4BogQEtt4APFYXqhc8PQuNw7tA/pub?gid=1430724446&single=true&output=csv")
+link_shop = shop_link["shop_link"].iloc[START_SHOP:]
 
 desired_caps = {
     "platformName": "Android",
@@ -119,13 +119,13 @@ for i in link_shop:
     try:
         driver.find_element(by=AppiumBy.XPATH, value=f"{OPEN_SHOP}").click()
     except: continue
-    time.sleep(3)
+    time.sleep(4)
     try: 
         driver.find_element(by=AppiumBy.XPATH, value='//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Categories"]').click()
         time.sleep(2)
         driver.find_element(by=AppiumBy.XPATH, value=f'//*[contains(@text,"{CATEGORY}")]').click()
     except: 
-        driver.find_element(by=AppiumBy.XPATH, value='//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Products"]').click()
+        continue
     time.sleep(3)
     try:
         driver.find_element(by=AppiumBy.XPATH, value='//com.lynx.tasm.behavior.ui.text.FlattenUIText[@content-desc="Bestsellers"]').click()
